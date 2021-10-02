2bb5d43fc609ed4122b15f1473d69dce461285b5
hugues96 <78543506+hugues96@users.noreply.github.com>
2021-10-02 10:27:58 +0200

    Add files via upload

---
+++ java_ism/src/entite/Etudiant.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entite;
+
+/**
+ *
+ * @author zidol
+ */
+public class Etudiant extends User {
+    private String matricule;
+    private String tuteur;
+    
+}
---
+++ java_ism/src/entite/Professeur.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entite;
+
+/**
+ *
+ * @author zidol
+ */
+public class Professeur extends User {
+    private int nci;
+    private String nomComplet;
+    private String grade;
+    
+}
